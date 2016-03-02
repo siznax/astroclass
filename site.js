9ebac90087399020f6dcdd0652ddd3dee910b45f
siznax <steve@siznax.net>
2016-03-02 15:53:44 -0800

    added toggle sections JS

--- site.js
+++ site.js
@@ -4,14 +4,30 @@
 
 var API = {
     "NASA": "https://api.nasa.gov/planetary/apod",
-    "Astrobin": "TBD"
+    "Astrobin": "http://astrobin.com/api/v1/"
 };
 var KEY = {
     "NASA": "vE9qJMI31OEuz1O8Wfwkvfj2Jl3mT23X3QgbSWZA",
-    "Astrobin": "TBD"
+    "Astrobin": {
+        "key": "7ee117a088915d6ed8886d38c70b7d1e83ef7e88",
+        "secret": "ea058d21015918d587c6e99d7a3107bb24bdb742"
+    }
 };
 
 $(function() {
+
+    // toggle sections
+    $(".button").click(function() {
+        var clicked = this.id;
+        $(".section").each(function() {
+            var selector = "#" + this.id + ".section";
+            if (this.id !== clicked) {
+                $(selector).css('display', 'none');
+            } else {
+                $(selector).css('display', 'block');
+            }
+        });
+    });
 
     // get APOD
     var call = API["NASA"] + "?api_key=" + KEY["NASA"];
